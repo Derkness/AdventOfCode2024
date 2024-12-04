3292c741f36ccbab76b472123b61011c76dec1bf
Derkness <tlpapae@gmail.com>
2024-12-04 19:42:27 +1030

    finished day 4

--- 4/main.py
+++ 4/main.py
@@ -1,7 +1,4 @@
-usedParts=[]
-
 def part_1():
-    global usedParts
     lines = []
     with open("input.txt", "r") as file:
         for line in file:
@@ -9,27 +6,15 @@ def part_1():
     count=0
     for x, line in enumerate(lines):
         for y, value in enumerate(line):
-            perIndex = 0
             if (value == 'X'):
                 for changeX in [-1, 0, 1]:
                     for changeY in [-1, 0, 1]:
                         if changeX == 0 and changeY == 0:
                             continue
-                        count+=look_along(lines, x, y, changeX, changeY)
-                        perIndex+=look_along(lines, x, y, changeX, changeY)
-            # print(x, y, perIndex)
-                        
-    # for x, line in enumerate(lines):
-    #     for y, value in enumerate(line):
-    #         if any(used[0] == x and used[1] == y for used in usedParts):
-    #             print(value, end="")
-    #         else:
-    #             print('.', end="")
-    #     print("")
+                        count+=look_along_pt_1(lines, x, y, changeX, changeY)
     return count
                 
-def look_along(lines, x, y, changeX, changeY):
-    global usedParts
+def look_along_pt_1(lines, x, y, changeX, changeY):
     if (x+(3*changeX) < 0 or y+(3*changeY) < 0):
         return 0
     if (x+(3*changeX) >= len(lines) or y+(3*changeY) >= len(lines[0])):
@@ -40,16 +25,67 @@ def look_along(lines, x, y, changeX, changeY):
         return 0
     if (lines[x+(3*changeX)][y+(3*changeY)] != 'S'):
         return 0
-    usedParts.append([x, y])
-    usedParts.append([x+(1*changeX), y+(1*changeY)])
-    usedParts.append([x+(2*changeX), y+(2*changeY)])
-    usedParts.append([x+(3*changeX), y+(3*changeY)])
     return 1
-             
+
+def part_2():
+    lines = []
+    with open("input.txt", "r") as file:
+        for line in file:
+            lines.append([x for x in line.strip()])
+    count=0
+    for x, line in enumerate(lines):
+        for y, value in enumerate(line):
+            if (value == 'A'):
+                count+=look_along_pt_2(lines, x, y)
+    return count
+                
+def look_along_pt_2(lines, x, y):
+    
+    # Check bounds
+    if (x-1 < 0 or y-1 < 0):
+        return 0
+    if (x+1 >= len(lines) or y+1 >= len(lines[0])):
+        return 0
+    
+    # Disqualify when an 'X' or an 'A' is in the cross
+    if (lines[x-1][y-1] == 'X' or lines[x+1][y-1] == 'X' or lines[x+1][y+1] == 'X' or lines[x-1][y+1] == 'X'):
+        return 0
+    if (lines[x-1][y-1] == 'A' or lines[x+1][y-1] == 'A' or lines[x+1][y+1] == 'A' or lines[x-1][y+1] == 'A'):
+        return 0
+    
+    # Check the MAS cross in a hideous but easily written way
+    if (lines[x-1][y-1] == "M"):
+        if (lines[x+1][y+1] != "S"):
+            return 0
+    if (lines[x-1][y-1] == "S"):
+        if (lines[x+1][y+1] != "M"):
+            return 0
+    
+    if (lines[x+1][y-1] == "M"):
+        if (lines[x-1][y+1] != "S"):
+            return 0
+    if (lines[x+1][y-1] == "S"):
+        if (lines[x-1][y+1] != "M"):
+            return 0
+    
+    if (lines[x+1][y+1] == "M"):
+        if (lines[x-1][y-1] != "S"):
+            return 0
+    if (lines[x+1][y+1] == "S"):
+        if (lines[x-1][y-1] != "M"):
+            return 0
+    
+    if (lines[x-1][y+1] == "M"):
+        if (lines[x+1][y-1] != "S"):
+            return 0
+    if (lines[x-1][y+1] == "S"):
+        if (lines[x+1][y-1] != "M"):
+            return 0
+        
+    # is valid!
+    return 1
+
 if __name__ == "__main__":
     totalValue = 0
-    # part_1()
-    print(part_1())
-
-# 2441 TOO HIGH
-# 2427 
+    print("part 1:", part_1())
+    print("part 2:", part_2())
